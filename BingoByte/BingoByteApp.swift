9557f1743a8c2206686ccf5a7c4b47b3b9eb6c58
Austin Lackey <austinblackey@gmail.com>
2026-02-07 22:35:18 -0700

    Initial Commit

---
+++ BingoByte/BingoByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingoByteApp.swift
+//  BingoByte
+//
+//  Created by Austin Lackey on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BingoByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
