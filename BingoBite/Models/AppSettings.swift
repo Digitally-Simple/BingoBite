f7c7471b154aaeebdbd3aaf3b88b82c1274e9449
Austin Lackey <austinblackey@gmail.com>
2026-04-12 16:20:56 -0600

    feat: add trialStartDate to AppSettings model

--- BingoBite/Models/AppSettings.swift
+++ BingoBite/Models/AppSettings.swift
@@ -6,10 +6,12 @@ final class AppSettings {
     var licenseKey: String = ""
     var licenseKeyInstanceId: String = ""
     var lastLicenseValidationDate: Date? = nil
+    var trialStartDate: Date? = nil
 
-    init(licenseKey: String = "", licenseKeyInstanceId: String = "", lastLicenseValidationDate: Date? = nil) {
+    init(licenseKey: String = "", licenseKeyInstanceId: String = "", lastLicenseValidationDate: Date? = nil, trialStartDate: Date? = nil) {
         self.licenseKey = licenseKey
         self.licenseKeyInstanceId = licenseKeyInstanceId
         self.lastLicenseValidationDate = lastLicenseValidationDate
+        self.trialStartDate = trialStartDate
     }
 }
